e42f6ac4807427b1271035508181ec48b88ea800
gladiee <gladwinwg123@gmail.com>
2023-11-26 10:48:34 +0530

    Its a First Change!

---
+++ Simple Python Programs (Basic-Starting-Programs!)/addition.py
@@ -0,0 +1,10 @@
+
+no1 = int(input("Enter First No: "))
+no2 = int(input("Enter Second NO: "))
+
+sum = no1 + no2
+
+print(no1,"+",no2,"=",sum)
+
+def add():
+    return print
---
+++ Simple Python Programs (Basic-Starting-Programs!)/multiply.py
@@ -0,0 +1,9 @@
+no1 = int(input("Enter First No: "))
+no2 = int(input("Enter Second NO: "))
+
+multiply = no1 * no2
+
+print(no1,"x",no2,"=",multiply)
+
+def multi():
+    return print
